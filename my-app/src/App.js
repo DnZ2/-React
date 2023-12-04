31c60c725db2265cf416a9ea2da18c1187e50863
Alexey Nikitenko <nikitenko_1504@mail.ru>
2023-12-04 16:44:35 +0300

    withoutJSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,60 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date(Date.now()).getFullYear()}</p>
-			</header>
-		</div>
-	);
+	return createElement(createWithoutJsx);
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 			<p>{new Date(Date.now()).getFullYear()}</p>
+	// 		</header>
+	// 	</div>
+	// );
 };
-export default App;
+
+function createWithoutJsx() {
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				{},
+				'Edit ',
+				createElement('code', {}, 'src/App.js'),
+				'  and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', {}, `${new Date(Date.now()).getFullYear()}`),
+		),
+	);
+}
